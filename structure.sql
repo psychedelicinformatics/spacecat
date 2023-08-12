0813c2234decb2ff9d2c0dd23f164d2cbdda6efe
ds <ds@electronic-minds.de>
2023-08-12 14:40:25 +0200

    Adds SQL für needed structure

---
+++ structure.sql
@@ -0,0 +1,27 @@
+SET
+SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET
+time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+CREATE TABLE `pixels`
+(
+    `id`       int NOT NULL,
+    `x`        int NOT NULL,
+    `y`        int NOT NULL,
+    `z`        int NOT NULL,
+    `todelete` int DEFAULT NULL,
+    `level`    int NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+ALTER TABLE `pixels`
+    ADD PRIMARY KEY (`id`);
+
+ALTER TABLE `pixels`
+    MODIFY `id` int NOT NULL AUTO_INCREMENT;
+COMMIT;
