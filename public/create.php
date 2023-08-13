0a4a31c5a6445775b7196c256a0aba8abcec116e
ds <ds@electronic-minds.de>
2023-08-13 21:06:17 +0200

    Makes levels in create.php consistent to output in index.php

--- public/create.php
+++ public/create.php
@@ -2,18 +2,13 @@
 
 declare(strict_types=1);
 
-use Generator\PyramidPixelGenerator;
-
 error_reporting(E_ALL);
 ini_set("display_errors", 1);
 
 require "../vendor/autoload.php";
 
-$floorGenerator = new PyramidPixelGenerator();
-$floorGenerator->createPyramid(4);
-$floorGenerator->createPyramid(6);
-$floorGenerator->createPyramid(7);
-$floorGenerator->cleanPixels();
-?>
+use Generator\PyramidPixelGenerator;
 
-test
+$floorGenerator = new PyramidPixelGenerator();
+$floorGenerator->createPyramid(8);
+$floorGenerator->cleanPixels();
